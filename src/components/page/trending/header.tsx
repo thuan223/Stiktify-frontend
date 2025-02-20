5302896eeba4530b05a9d81b4747c505ed6ba004
thuan223 <thuantnn.it@gmail.com>
2025-02-20 15:43:06 +0700

    fix conflict

--- src/components/page/trending/header.tsx
+++ src/components/page/trending/header.tsx
@@ -29,10 +29,10 @@ const Header: React.FC<HeaderProps> = ({
           target="_blank"
           rel=""
           onClick={({}) => {
-            router.push("/page/my-video");
+            router.push("/page/my-profile");
           }}
         >
-          My videos
+          My Profile
         </a>
       ),
     },
