646abfd2b06674136410dfff8f085d5d8bc3165c
Quoc Hao <haotqce170322@fpt.edu.vn>
2025-03-26 14:42:38 +0700

    done video report

--- src/actions/manage.report.action.tsx
+++ src/actions/manage.report.action.tsx
@@ -1,111 +1,159 @@
-"use server"
+"use server";
 
 import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
-const cookieStore = cookies()
-const token = cookieStore.get("token")?.value
+const cookieStore = cookies();
+const token = cookieStore.get("token")?.value;
 
-export const handleGetAllReportAction = async (current: string, pageSize: string) => {
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/list-report?current=${current}&pageSize=${pageSize}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            next: { tags: ["list-report"] }
-        })
-        const result: IBackendRes<any> = await res.json();
-        return result
-    } catch (error) {
-        return null
-    }
-}
-
-export const handleGetAllReportMusicAction = async (current: string, pageSize: string) => {
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/list-report-music?current=${current}&pageSize=${pageSize}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
-            next: { tags: ["list-report-music"] }
-        })
-        const result: IBackendRes<any> = await res.json();
-        return result
-    } catch (error) {
-        return null
-    }
-}
-
-export const handleDeleteReportVideoAction = async (id: string) => {
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/delete-report/${id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-                "Authorization": `Bearer ${token}`
-            },
-        })
-
-        revalidateTag("list-report")
-        const result: IBackendRes<any> = await res.json();
-        return result
-    } catch (error) {
-        return null
-    }
-}
-
-export const handleFlagMusicAction = async (id: string, flag: boolean) => {
-    try {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/musics/flag-music`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    _id: id,
-                    flag: flag,
-                }),
-            }
-        );
-        revalidateTag("list-music");
-        revalidateTag("list-report-music");
-        const result: IBackendRes<any> = await res.json();
-        return result;
-    } catch (error) {
-        return null;
-    }
+export const handleGetAllReportAction = async (
+  current: string,
+  pageSize: string
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/list-report?current=${current}&pageSize=${pageSize}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["list-report"] },
+      }
+    );
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
 };
 
-    export const handleSearchMusicReportAction = async (
-        search: string, 
-        startDate?: string, 
-    ) => {
-        try {
-        const query = new URLSearchParams();
-        query.append('search', search);
-        if (startDate) query.append('startDate', startDate);
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/search-music?${query.toString()}`, {
-            method: 'GET',
-            headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-            },
-            next: { tags: ['list-report-music'] },
-        });
-        const result: IBackendRes<any> = await res.json();
-        return result;
-        } catch (error) {
-        console.error('Error searching music report:', error);
-        return null;
-        }
-    }
-  
+export const handleGetAllReportMusicAction = async (
+  current: string,
+  pageSize: string
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/list-report-music?current=${current}&pageSize=${pageSize}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["list-report-music"] },
+      }
+    );
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const handleDeleteReportVideoAction = async (id: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/report/delete-report/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    revalidateTag("list-report");
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const handleFlagMusicAction = async (id: string, flag: boolean) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/musics/flag-music`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          _id: id,
+          flag: flag,
+        }),
+      }
+    );
+    revalidateTag("list-music");
+    revalidateTag("list-report-music");
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const handleSearchMusicReportAction = async (
+  search: string,
+  startDate?: string
+) => {
+  try {
+    const query = new URLSearchParams();
+    query.append("search", search);
+    if (startDate) query.append("startDate", startDate);
+    const res = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_BACKEND_URL
+      }/api/v1/report/search-music?${query.toString()}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["list-report-music"] },
+      }
+    );
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    console.error("Error searching music report:", error);
+    return null;
+  }
+};
+
+export const handleSearchVideoReportAction = async (
+  search: string,
+  startDate?: string
+) => {
+  try {
+    const query = new URLSearchParams();
+    query.append("search", search);
+    if (startDate) query.append("startDate", startDate);
+    const res = await fetch(
+      `${
+        process.env.NEXT_PUBLIC_BACKEND_URL
+      }/api/v1/report/search-video?${query.toString()}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        next: { tags: ["list-report-video"] },
+      }
+    );
+    const result: IBackendRes<any> = await res.json();
+    return result;
+  } catch (error) {
+    console.error("Error searching video report:", error);
+    return null;
+  }
+};
--- src/components/admin/report.video.table.tsx
+++ src/components/admin/report.video.table.tsx
@@ -1,147 +1,250 @@
-"use client"
+"use client";
 import { ColumnsType } from "antd/es/table";
-import TableCustomize from "../table/table.dashboard"
-import { useState } from "react";
+import TableCustomize from "../table/table.dashboard";
+import { useEffect, useState } from "react";
 import { formatNumber } from "@/utils/utils";
-import { DeleteTwoTone, FlagTwoTone, UnorderedListOutlined } from "@ant-design/icons";
-import { Button, Image, notification, Popconfirm, Tooltip } from "antd";
+import {
+  DeleteTwoTone,
+  FlagTwoTone,
+  SearchOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import {
+  Button,
+  DatePicker,
+  DatePickerProps,
+  notification,
+  Popconfirm,
+  Tooltip,
+} from "antd";
 import { handleFlagShortVideoAction } from "@/actions/manage.short.video.action";
 import VideoCustomize from "../video/video.customize";
 import ModalListReport from "../modal/modal.list.report";
-import { handleDeleteReportVideoAction } from "@/actions/manage.report.action";
+import {
+  handleDeleteReportVideoAction,
+  handleSearchVideoReportAction,
+} from "@/actions/manage.report.action";
+import InputCustomize from "../input/input.customize";
+import dayjs from "dayjs";
 
 interface IProps {
-    dataSource: IReport[];
-    meta: {
-        current: number,
-        pageSize: number,
-        total: number,
-    }
+  dataSource: IReport[];
+  meta: {
+    current: number;
+    pageSize: number;
+    total: number;
+  };
 }
-
 
 const ManageReportTable = (props: IProps) => {
-    const { dataSource, meta } = props;
-    const [isReportModalOpen, setIsReportModalOpen] = useState<boolean>(false)
-    const [dataReport, setDataReport] = useState<IDataReport[] | []>([])
+  const { dataSource, meta } = props;
+  const [isReportModalOpen, setIsReportModalOpen] = useState<boolean>(false);
+  const [dataReport, setDataReport] = useState<IDataReport[] | []>([]);
+  const [startDate, setStartDate] = useState<string | undefined>(undefined);
+  const [dataTable, setDataTable] = useState<IReport[] | []>(dataSource);
+  const [metaTable, setMetaTable] = useState(meta);
+  const [search, setSearch] = useState("");
 
-    const handleFlagVideo = async (record: IShortVideo) => {
-        const res = await handleFlagShortVideoAction(record._id, !record.flag)
-        if (res?.statusCode === 201) {
-            return notification.success({ message: res?.message })
-        }
-        return notification.error({ message: res?.message })
+  const handleFlagVideo = async (record: IShortVideo) => {
+    const res = await handleFlagShortVideoAction(record._id, !record.flag);
+    if (res?.statusCode === 201) {
+      notification.success({ message: res?.message });
+    } else {
+      notification.error({ message: res?.message });
     }
+  };
 
-    const handleDeleteReportVideo = async (id: string) => {
-        const res = await handleDeleteReportVideoAction(id)
+  const handleDeleteReportVideo = async (id: string) => {
+    const res = await handleDeleteReportVideoAction(id);
+    if (res?.statusCode === 200) {
+      notification.success({ message: res?.message });
+    } else {
+      notification.error({ message: res?.message });
+    }
+  };
+
+  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (search.length > 0 || startDate) {
+        const res = await handleSearchVideoReportAction(search, startDate);
         if (res?.statusCode === 200) {
-            return notification.success({ message: res?.message })
-        }
-        return notification.error({ message: res?.message })
-    }
-
-    const columns: ColumnsType<IReport> = [
-        {
-            title: 'Username',
-            dataIndex: 'dataVideo',
-            key: 'userName',
-            render: (value, record, index) => (
-                <div>{record.dataVideo.userId.userName}</div>
-            )
-        },
-        {
-            title: 'Video',
-            dataIndex: 'dataVideo',
-            key: 'videoThumbnail',
-            render: (value: IShortVideo, record, index) => (
-                <VideoCustomize videoThumbnail={value.videoThumbnail} videoUrl={value.videoUrl} />
-            ),
-        },
-        {
-            title: 'Views',
-            dataIndex: 'dataVideo',
-            key: 'totalViews',
-            render: (value: IShortVideo, record, index) => (
-                <div>{formatNumber(value.totalViews ?? 0)}</div>
-            )
-        },
-        {
-            title: 'Total Report',
-            dataIndex: 'total',
-            key: 'total',
-            render: (value, record, index) => (
-                <div>{formatNumber(value ?? 0)}</div>
-            )
-        },
-        {
-            title: 'Reasons',
-            dataIndex: 'dataReport',
-            key: 'dataReport',
-            render: (value, record, index) => (
-                <>
-                    <Tooltip overlayInnerStyle={{ background: "white", color: "#1e272e" }} title="Click here to show list report">
-                        <Button onClick={() => {
-                            setIsReportModalOpen(true)
-                            setDataReport(value)
-                        }}><UnorderedListOutlined /></Button>
-                    </Tooltip>
-                </>
-            )
-        },
-        {
-            title: 'Action',
-            dataIndex: 'dataVideo',
-            key: 'flag',
-            render: (value: IShortVideo, record, index) => {
-                return (
-                    <div style={{ display: "flex", gap: 10 }}>
-                        <Popconfirm
-                            title="Sure to flag video?"
-                            onConfirm={() => handleFlagVideo(value)}
-                            okText="Yes"
-                            cancelText="No"
-                        >
-                            <FlagTwoTone
-                                style={{ fontSize: "20px" }}
-                                twoToneColor={value.flag ? "#ff7675" : ""} />
-                        </Popconfirm>
-                        <Popconfirm
-                            title="Sure to delete video report?"
-                            onConfirm={() => handleDeleteReportVideo(record.dataVideo._id)}
-                            okText="Yes"
-                            cancelText="No"
-                        >
-                            <DeleteTwoTone
-                                style={{ fontSize: "20px" }}
-                                twoToneColor={"#ff7675"} />
-                        </Popconfirm>
-                    </div>
-                )
+          if (res.data?.result?.length === 0) {
+            notification.info({
+              message: startDate
+                ? `No reports found for ${startDate}.`
+                : "No reports found for the search term.",
+            });
+          }
+          const mappedData = res.data?.result?.map((item: any) => ({
+            _id: item._id,
+            dataVideo: {
+              _id: item.videoId._id,
+              videoUrl: item.videoId.videoUrl,
+              videoDescription: item.videoId.videoDescription,
+              videoThumbnail: item.videoId.videoThumbnail,
+              totalViews: item.videoId.totalViews || 0, // Sửa từ totalListener thành totalViews
+              flag: item.videoId.flag || false,
+              userId: {
+                _id: item.videoId.userId._id,
+                userName: item.videoId.userId.userName,
+              },
             },
+            total: item.total || 1,
+            dataReport: item.dataReport || [
+              {
+                userName: item.userId.userName,
+                reason: item.reasons,
+              },
+            ],
+          }));
+          setDataTable(mappedData || []);
+          setMetaTable({
+            current: res.data?.meta?.current || 1,
+            pageSize: res.data?.meta?.pageSize || 10,
+            total: res.data?.meta?.total || 0,
+          });
+        } else {
+          notification.error({ message: "Failed to fetch video reports." });
         }
-    ];
+      } else {
+        setDataTable(dataSource);
+        setMetaTable(meta);
+      }
+    })();
+  }, [search, startDate, dataSource]);
 
-    return (
-        <>
-            <div style={{
-                display: "flex", justifyContent: "space-between",
-                alignItems: "center",
-                marginBottom: 20,
-                fontWeight: 600,
-                fontSize: 20
-            }}>
-                <span>Manager Report Video</span>
-                {/* <Button onClick={() => setIsCreateModalOpen(true)}>
-                    <UserAddOutlined />
-                    <span>Add New</span>
-                </Button> */}
-            </div >
-            <TableCustomize columns={columns} dataSource={dataSource} meta={meta} />
-            <ModalListReport data={dataReport} isModalOpen={isReportModalOpen} setIsModalOpen={setIsReportModalOpen} />
+  const columns: ColumnsType<IReport> = [
+    {
+      title: "Username",
+      dataIndex: "dataVideo",
+      key: "userName",
+      render: (value, record) => <div>{record.dataVideo.userId.userName}</div>,
+    },
+    {
+      title: "Video",
+      dataIndex: "dataVideo",
+      key: "videoThumbnail",
+      render: (value: IShortVideo) => (
+        <VideoCustomize
+          videoThumbnail={value.videoThumbnail}
+          videoUrl={value.videoUrl}
+        />
+      ),
+    },
+    {
+      title: "Views",
+      dataIndex: "dataVideo",
+      key: "totalViews",
+      render: (value: IShortVideo) => (
+        <div>{formatNumber(value.totalViews ?? 0)}</div>
+      ),
+    },
+    {
+      title: "Total Report",
+      dataIndex: "total",
+      key: "total",
+      render: (value) => <div>{formatNumber(value ?? 0)}</div>,
+    },
+    {
+      title: "Reasons",
+      dataIndex: "dataReport",
+      key: "dataReport",
+      render: (value) => (
+        <Tooltip
+          overlayInnerStyle={{ background: "white", color: "#1e272e" }}
+          title="Click here to show list report"
+        >
+          <Button
+            onClick={() => {
+              setIsReportModalOpen(true);
+              setDataReport(value);
+            }}
+          >
+            <UnorderedListOutlined />
+          </Button>
+        </Tooltip>
+      ),
+    },
+    {
+      title: "Action",
+      dataIndex: "dataVideo",
+      key: "flag",
+      render: (value: IShortVideo) => (
+        <div style={{ display: "flex", gap: 10 }}>
+          <Popconfirm
+            title="Sure to flag video?"
+            onConfirm={() => handleFlagVideo(value)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <FlagTwoTone
+              style={{ fontSize: "20px" }}
+              twoToneColor={value.flag ? "#ff7675" : ""}
+            />
+          </Popconfirm>
+          <Popconfirm
+            title="Sure to delete video report?"
+            onConfirm={() => handleDeleteReportVideo(value._id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <DeleteTwoTone
+              style={{ fontSize: "20px" }}
+              twoToneColor={"#ff7675"}
+            />
+          </Popconfirm>
+        </div>
+      ),
+    },
+  ];
 
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 20,
+          fontWeight: 600,
+          fontSize: 20,
+        }}
+      >
+        <span>Manager Report Video</span>
+      </div>
+      <div style={{ width: "300px" }}>
+        <div>
+          <InputCustomize
+            setValue={setSearch}
+            value={search}
+            icon={<SearchOutlined />}
+          />
+        </div>
+        <div
+          style={{ width: "130px", marginLeft: "310px", marginTop: "-32px" }}
+        >
+          <DatePicker
+            onChange={onChange}
+            value={startDate ? dayjs(startDate, "YYYY-MM-DD") : null}
+          />
+        </div>
+      </div>
+      <TableCustomize
+        columns={columns}
+        dataSource={dataTable}
+        meta={metaTable}
+      />
+      <ModalListReport
+        data={dataReport}
+        isModalOpen={isReportModalOpen}
+        setIsModalOpen={setIsReportModalOpen}
+      />
+    </>
+  );
+};
 
-export default ManageReportTable
+export default ManageReportTable;
